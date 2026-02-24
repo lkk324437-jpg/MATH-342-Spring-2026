658d57554abfe7d63ec9580ac03c730af67ba942
lkk324437-jpg <lkk324437@gmail.com>
2026-02-24 22:11:08 +0000

    Latest build deployed.

--- week1/lunr-pretext-search-index.js
+++ week1/lunr-pretext-search-index.js
@@ -1,121 +1,130 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "handouts",
+  "id": "week1-handout",
   "level": "1",
-  "url": "handouts.html",
-  "type": "Chapter",
+  "url": "week1-handout.html",
+  "type": "Worksheet",
   "number": "",
-  "title": "Handouts",
-  "body": " Handouts    PreTeXt  "
+  "title": "Week 1 Handout- Introduction to MATH 342 and Laplace Transform",
+  "body": " Week 1 Handout- Introduction to MATH 342 and Laplace Transform   This is an outline of the topics we covered in the first week of class. You should have Jupyter Notebook , MATHEMATICA , and Octave ready before the 2nd week for practicing and doing assignments.     Overview   Investigating high dimensional systems using vector analysis  Constructing complicated functions using series expansion  Modeling and studying natural phenomena via partial differential equations (PDE)  Solving PDEs algebraically and numerically  Utilizing appropriate software packages ( Jupyter Notebook , MATHEMATICA , Octave ) for suitable tasks    Why do we need models?   All models are wrong. Some are useful.    Steps to create models:   Observing and recording the system  Describe how the system behaves (verbally and mathematically)-create a model  Verify whether that model behaves similar (or close enough) to what you observe. If not, modify it.  Investigate\/Study the system under different conditions using the model     COMAP    Laplace Transform: Definitions, Theorems, Examples, MATHEMATICA      If is a function defined for , then the integral is the Laplace transform of provided the integral converges.  Sufficient conditions for existence:    must be piecewise continuous on     must be of exponential order for          A function is said to be exponential order if there exists constants and such that       Determine whether the Laplace transform of each of the functions exists. Justify your answer.                          is a linear transform, i.e.,         Evaluate     Evaluate where   Graph of a piecewise function   The graph of a piecewise function with two horizontal segments. From t equals 0 to t equals 3, the graph is on the t-axis at y equals 0, ending with an open circle at (3, 0). For t greater than or equal to 3, the graph is a horizontal line at y equals 2, starting with a closed circle at (3, 2).             If represents the Laplace transform of a function then is the inverse Laplace transform of     is a linear transform,                                        Use Theorem to find each of the followings:                Find      Conceptually, the original DE is transformed to another equation in a different space. Ideally, this new equation is easier to solve in the new space. Finally, the solution in the new space is transformed back to the original space using the inverse transformation.   Steps in solving an IVP by the Laplace transform      There are many other transformations and each one is considering a different space, which also means each one is more suitable for solving some particular types of DEs. This is a typical issue for most of algebraic approaches for solving ODEs or PDEs.     Suppose are continuous on , is piecewise continuous on and are of exponential order with as specified in . Then for  where     MATHEMATICA     You should be familiar with using MATHEMATICA to verify your algebraic work.     LaplaceTransform[f(t),t,s]   Find using MATHEMATICA.  LaplaceTransform[Cos[4*t - 5], t, s]     InverseLaplaceTransform[F[s],s,t]  Find using MATHEMATICA.  InverseLaplaceTransform[((5*s-1)\/(s^2+7), s, t]         Use the Laplace transform to solve the IVP    "
 },
 {
-  "id": "week3-handout-2-1-1-1",
+  "id": "def-laplace",
   "level": "2",
-  "url": "week3-handout.html#week3-handout-2-1-1-1",
-  "type": "Paragraph (with a defined term)",
-  "number": "",
-  "title": "",
-  "body": "Vector calculus (Chap 9 in the textbook) "
-},
-{
-  "id": "def-gradient",
-  "level": "2",
-  "url": "week3-handout.html#def-gradient",
+  "url": "week1-handout.html#def-laplace",
   "type": "Definition",
   "number": "1",
   "title": "",
-  "body": "  When the vector differential operator , is applied to a differentiable function we say that the vector is the gradient of the respective function    "
+  "body": "  If is a function defined for , then the integral is the Laplace transform of provided the integral converges.  Sufficient conditions for existence:    must be piecewise continuous on     must be of exponential order for       "
 },
 {
-  "id": "def-directional",
+  "id": "def-exponential-order",
   "level": "2",
-  "url": "week3-handout.html#def-directional",
+  "url": "week1-handout.html#def-exponential-order",
   "type": "Definition",
   "number": "2",
   "title": "",
-  "body": "  The directional derivative of in the direction of a unit vector is provided the limit exists.   "
+  "body": "  A function is said to be exponential order if there exists constants and such that    "
 },
 {
-  "id": "thm-compu-directional",
+  "id": "week1-handout-6",
   "level": "2",
-  "url": "week3-handout.html#thm-compu-directional",
-  "type": "Theorem",
+  "url": "week1-handout.html#week1-handout-6",
+  "type": "Example",
   "number": "3",
   "title": "",
-  "body": "  If is a differentiable function of and and then Similarly, for a function and where and are the direction angles of the unit vector measured relative to the positive and axes, respectively, it can be shown that    "
+  "body": "  Determine whether the Laplace transform of each of the functions exists. Justify your answer.                       "
 },
 {
-  "id": "week3-handout-6",
+  "id": "week1-handout-7",
   "level": "2",
-  "url": "week3-handout.html#week3-handout-6",
+  "url": "week1-handout.html#week1-handout-7",
   "type": "Remark",
   "number": "4",
   "title": "",
-  "body": "  The gradient vector points in the direction in which increases most rapidly, whereas points in the direction of the most rapid decrease of    "
+  "body": "  is a linear transform, i.e.,   "
 },
 {
-  "id": "week3-handout-7",
+  "id": "week1-handout-8",
   "level": "2",
-  "url": "week3-handout.html#week3-handout-7",
+  "url": "week1-handout.html#week1-handout-8",
   "type": "Example",
   "number": "5",
   "title": "",
-  "body": "  The temperature in a rectangular box is approximated by If a mosquito is located at in which direction should it fly to cool off as rapidly as possible?   "
+  "body": "     Evaluate     Evaluate where   Graph of a piecewise function   The graph of a piecewise function with two horizontal segments. From t equals 0 to t equals 3, the graph is on the t-axis at y equals 0, ending with an open circle at (3, 0). For t greater than or equal to 3, the graph is a horizontal line at y equals 2, starting with a closed circle at (3, 2).          "
 },
 {
-  "id": "week3-handout-8",
+  "id": "def-laplace-inverse",
   "level": "2",
-  "url": "week3-handout.html#week3-handout-8",
-  "type": "Example",
-  "number": "6",
-  "title": "",
-  "body": "  The temperature at a point in space is inversely proportional to the square of the distance from to the origin. It is known that Find the rate of change of at in the direction of In which direction form does the temperature increase most rapidly? At what is the maximum rate of change of ?   "
-},
-{
-  "id": "week3-handout-9",
-  "level": "2",
-  "url": "week3-handout.html#week3-handout-9",
-  "type": "Remark",
+  "url": "week1-handout.html#def-laplace-inverse",
+  "type": "Definition",
   "number": "7",
   "title": "",
-  "body": "  Let be the level surface of a differentiable function that passes through If the differentiable function are the parametric equation of a curve on the surface for which The derivative of implies that i.e., is normal to the level surface at    "
+  "body": "  If represents the Laplace transform of a function then is the inverse Laplace transform of     is a linear transform,    "
 },
 {
-  "id": "thm-tangent-plane",
+  "id": "thm-laplace-inverse",
   "level": "2",
-  "url": "week3-handout.html#thm-tangent-plane",
+  "url": "week1-handout.html#thm-laplace-inverse",
   "type": "Theorem",
   "number": "8",
   "title": "",
-  "body": "  Let be a point on the graph of where is not Then an equation of the tangent plane at is Furthermore, the line containing that is parallel to is called the normal line to the surface at P, which can be expressed as symmetric equations  or parametric equations     "
+  "body": "                                 "
 },
 {
-  "id": "week3-handout-11",
+  "id": "week1-handout-11",
   "level": "2",
-  "url": "week3-handout.html#week3-handout-11",
+  "url": "week1-handout.html#week1-handout-11",
   "type": "Example",
   "number": "9",
   "title": "",
-  "body": "  Given and Answer the followings:   Sketch the level surface passing through the indicated point.    Sketch the gradient at the point.    Find an equation of the tangent plane at the indicated point.   Find parametric equations and symmetric equations for the normal line at the indicated point      "
+  "body": " Use Theorem to find each of the followings:             "
 },
 {
-  "id": "def-vector-fields",
+  "id": "week1-handout-12",
   "level": "2",
-  "url": "week3-handout.html#def-vector-fields",
-  "type": "Definition",
+  "url": "week1-handout.html#week1-handout-12",
+  "type": "Example",
   "number": "10",
   "title": "",
-  "body": "  Vector functions of two and three variables,    "
+  "body": "  Find    "
 },
 {
-  "id": "week3-handout-13",
+  "id": "week1-handout-13",
   "level": "2",
-  "url": "week3-handout.html#week3-handout-13",
-  "type": "Definition",
+  "url": "week1-handout.html#week1-handout-13",
+  "type": "Remark",
   "number": "11",
   "title": "",
-  "body": "  The curl of a vector field is the vector field It can be computed from the cross product,    "
+  "body": " Conceptually, the original DE is transformed to another equation in a different space. Ideally, this new equation is easier to solve in the new space. Finally, the solution in the new space is transformed back to the original space using the inverse transformation.   Steps in solving an IVP by the Laplace transform      There are many other transformations and each one is considering a different space, which also means each one is more suitable for solving some particular types of DEs. This is a typical issue for most of algebraic approaches for solving ODEs or PDEs.  "
+},
+{
+  "id": "thm-laplace-properties",
+  "level": "2",
+  "url": "week1-handout.html#thm-laplace-properties",
+  "type": "Theorem",
+  "number": "13",
+  "title": "",
+  "body": "  Suppose are continuous on , is piecewise continuous on and are of exponential order with as specified in . Then for  where    "
+},
+{
+  "id": "week1-handout-15",
+  "level": "2",
+  "url": "week1-handout.html#week1-handout-15",
+  "type": "Remark",
+  "number": "14",
+  "title": "<em class=\"alert\">MATHEMATICA<\/em>.",
+  "body": "MATHEMATICA     You should be familiar with using MATHEMATICA to verify your algebraic work.     LaplaceTransform[f(t),t,s]   Find using MATHEMATICA.  LaplaceTransform[Cos[4*t - 5], t, s]     InverseLaplaceTransform[F[s],s,t]  Find using MATHEMATICA.  InverseLaplaceTransform[((5*s-1)\/(s^2+7), s, t]       "
+},
+{
+  "id": "week1-handout-16",
+  "level": "2",
+  "url": "week1-handout.html#week1-handout-16",
+  "type": "Example",
+  "number": "17",
+  "title": "",
+  "body": " Use the Laplace transform to solve the IVP   "
 }
 ]
 
--- week7/lunr-pretext-search-index.js
+++ week7/lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet",
   "number": "",
   "title": "Week 7 Handout- Series Solution-Part 3",
-  "body": " Week 7 Handout- Series Solution-Part 3    We will discuss the following topics related to Series Solution :   Week 5: Power Series about Ordinary Points and Singular Points (Section 5.1-5.2)    Week 6-7: Orthogonal Functions (Section 12.1), Legendre Function (Section 5.3), Fourier Series (Section 12.2-12.3)            is the imaginary unit defined by    For a real number, Euler's formula    A complex number is a number where and are real numbers. The number is called the conjugate of          From , we can derive Then using to replace and in the defintion of Fourier Series.     Complex Fourier Series   The complex Fourier series of function defined on an interval is given by If is a periodic function and the fundamental period of is then becomes where is called fundamental angular frequency and is called frequency spectrum of       Find the complex Fourier series of on the given interval    Graph the periodic expansion of     Graph Frequency spectrum of    Sketch the graph of partial sum for        "
+  "body": " Week 7 Handout- Series Solution-Part 3    We will discuss the following topics related to Series Solution :   Week 5: Power Series about Ordinary Points and Singular Points (Section 5.1-5.2)    Week 6-7: Orthogonal Functions (Section 12.1), Legendre Function (Section 5.3), Fourier Series (Section 12.2-12.4), Sturm-Liouville Problem (Section 12.5)            is the imaginary unit defined by    For a real number, Euler's formula    A complex number is a number where and are real numbers. The number is called the conjugate of          From , we can derive Then using to replace and in the defintion of Fourier Series.     Complex Fourier Series   The complex Fourier series of function defined on an interval is given by If is a periodic function and the fundamental period of is then becomes where is called fundamental angular frequency and is called frequency spectrum of       Find the complex Fourier series of on the given interval    Graph the periodic expansion of     Graph Frequency spectrum of    Sketch the graph of partial sum for          Let and be real-valued functions continuous on an interval , and let and for every in the interval. is said to be a regular Sturm-Liouville Problem.     Solve the boundary value problem   "
 },
 {
   "id": "week5-handout-2-1-1",
@@ -53,6 +53,24 @@ var ptx_lunr_docs = [
   "number": "4",
   "title": "",
   "body": "  Find the complex Fourier series of on the given interval    Graph the periodic expansion of     Graph Frequency spectrum of    Sketch the graph of partial sum for       "
+},
+{
+  "id": "def-sturm-liouville",
+  "level": "2",
+  "url": "week5-handout.html#def-sturm-liouville",
+  "type": "Definition",
+  "number": "5",
+  "title": "",
+  "body": "  Let and be real-valued functions continuous on an interval , and let and for every in the interval. is said to be a regular Sturm-Liouville Problem.    "
+},
+{
+  "id": "week5-handout-8",
+  "level": "2",
+  "url": "week5-handout.html#week5-handout-8",
+  "type": "Example",
+  "number": "6",
+  "title": "",
+  "body": "Solve the boundary value problem  "
 }
 ]
 
