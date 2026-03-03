b6f052635b9a191105a19a2e83d49dc458ee1843
lkk324437-jpg <lkk324437@gmail.com>
2026-03-03 14:12:33 +0000

    Latest build deployed.

--- week7/lunr-pretext-search-index.js
+++ week7/lunr-pretext-search-index.js
@@ -6,8 +6,8 @@ var ptx_lunr_docs = [
   "url": "week7-handout.html",
   "type": "Worksheet",
   "number": "",
-  "title": "Week 7 Handout- Series Solution-Part 3",
-  "body": " Week 7 Handout- Series Solution-Part 3    We will discuss the following topics related to Series Solution :   Week 5: Power Series about Ordinary Points and Singular Points (Section 5.1-5.2)    Week 6-7: Orthogonal Functions (Section 12.1), Legendre Function (Section 5.3), Fourier Series (Section 12.2-12.4), Sturm-Liouville Problem (Section 12.5)            is the imaginary unit defined by    For a real number, Euler's formula    A complex number is a number where and are real numbers. The number is called the conjugate of          From , we can derive Then using to replace and in the defintion of Fourier Series.     Complex Fourier Series   The complex Fourier series of function defined on an interval is given by If is a periodic function and the fundamental period of is then becomes where is called fundamental angular frequency and is called frequency spectrum of       Find the complex Fourier series of on the given interval    Graph the periodic expansion of     Graph Frequency spectrum of    Sketch the graph of partial sum for          Let and be real-valued functions continuous on an interval , and let and for every in the interval. is said to be a regular Sturm-Liouville Problem.     Solve the boundary value problem   "
+  "title": "Week 7-8 Handout- Series Solution-Part 3",
+  "body": " Week 7-8 Handout- Series Solution-Part 3    We will discuss the following topics related to Series Solution :   Week 5: Power Series about Ordinary Points and Singular Points (Section 5.1-5.2)    Week 6-7: Orthogonal Functions (Section 12.1), Legendre Function (Section 5.3), Fourier Series (Section 12.2-12.4)    Week 7-Week 8: Boundary Value Problems in Rectangular Coordinates (13.1-13.5)           is the imaginary unit defined by    For a real number, Euler's formula    A complex number is a number where and are real numbers. The number is called the conjugate of          From , we can derive Then using to replace and in the defintion of Fourier Series.     Complex Fourier Series   The complex Fourier series of function defined on an interval is given by If is a periodic function and the fundamental period of is then becomes where is called fundamental angular frequency and is called frequency spectrum of       Find the complex Fourier series of on the given interval    Graph the periodic expansion of     Graph Frequency spectrum of    Sketch the graph of partial sum for         Linear Partial Differential Equation   Let denote the dependent variable and and independent variables, the general form of a linear second-order partial differential equations is given by where the coefficients are constants or functions of and When , is said to be homogeneous ; otherwise, it is nonhomogeneous.      Separation of Variables  While seeking a particular solution of a differential equation in which the independent variable are and , we assume that this particular solution is in the form of product of a function of and a function of :       Use Separation of Variables to solve      Superposition Principle   If are solutions of a homogeneous linear PDE, then the linear combination is also a solution, where are constants.    Many other approaches are also based on the above theorem, for example, power series, Fourier series, etc.    In power series, what is ?    In Fourier series, what is ?      Classification of linear 2nd-order PDEs   The linear 2nd order PDE: is said to be   hyperbolic if    parabolic if    elliptic if          Classify each of given PDEs as hyperbolic, parabolic, or elliptic.                   Here, we focus on finding product solutions of the 2nd-order PDEs and observe the relationship with Fourier series:  1-D Heat Equation:     1D Wave Equation:   Laplace's Equation: where or         1-D Heat Equation   Consider a thin rod of length with an initial temperature throughout and whose ends are held at temperature zero for all time   A thin rod .     The temperature in the rod is determined from the BVP:     "
 },
 {
   "id": "week7-handout-2-1-1",
@@ -55,22 +55,85 @@ var ptx_lunr_docs = [
   "body": "  Find the complex Fourier series of on the given interval    Graph the periodic expansion of     Graph Frequency spectrum of    Sketch the graph of partial sum for       "
 },
 {
-  "id": "def-sturm-liouville",
+  "id": "def-2nd-PDE",
   "level": "2",
-  "url": "week7-handout.html#def-sturm-liouville",
+  "url": "week7-handout.html#def-2nd-PDE",
   "type": "Definition",
   "number": "5",
-  "title": "",
-  "body": "  Let and be real-valued functions continuous on an interval , and let and for every in the interval. is said to be a regular Sturm-Liouville Problem.    "
+  "title": "Linear Partial Differential Equation.",
+  "body": " Linear Partial Differential Equation   Let denote the dependent variable and and independent variables, the general form of a linear second-order partial differential equations is given by where the coefficients are constants or functions of and When , is said to be homogeneous ; otherwise, it is nonhomogeneous.    "
 },
 {
-  "id": "week7-handout-8",
+  "id": "def-sep-var",
   "level": "2",
-  "url": "week7-handout.html#week7-handout-8",
-  "type": "Example",
+  "url": "week7-handout.html#def-sep-var",
+  "type": "Definition",
   "number": "6",
+  "title": "Separation of Variables.",
+  "body": " Separation of Variables  While seeking a particular solution of a differential equation in which the independent variable are and , we assume that this particular solution is in the form of product of a function of and a function of :    "
+},
+{
+  "id": "week7-handout-9",
+  "level": "2",
+  "url": "week7-handout.html#week7-handout-9",
+  "type": "Example",
+  "number": "7",
   "title": "",
-  "body": "Solve the boundary value problem  "
+  "body": "  Use Separation of Variables to solve    "
+},
+{
+  "id": "thm-superposition",
+  "level": "2",
+  "url": "week7-handout.html#thm-superposition",
+  "type": "Theorem",
+  "number": "8",
+  "title": "Superposition Principle.",
+  "body": " Superposition Principle   If are solutions of a homogeneous linear PDE, then the linear combination is also a solution, where are constants.   "
+},
+{
+  "id": "week7-handout-11",
+  "level": "2",
+  "url": "week7-handout.html#week7-handout-11",
+  "type": "Remark",
+  "number": "9",
+  "title": "",
+  "body": "Many other approaches are also based on the above theorem, for example, power series, Fourier series, etc.    In power series, what is ?    In Fourier series, what is ?    "
+},
+{
+  "id": "def-classification-2nd-pdes",
+  "level": "2",
+  "url": "week7-handout.html#def-classification-2nd-pdes",
+  "type": "Definition",
+  "number": "10",
+  "title": "Classification of linear 2nd-order PDEs.",
+  "body": " Classification of linear 2nd-order PDEs   The linear 2nd order PDE: is said to be   hyperbolic if    parabolic if    elliptic if       "
+},
+{
+  "id": "week7-handout-13",
+  "level": "2",
+  "url": "week7-handout.html#week7-handout-13",
+  "type": "Example",
+  "number": "11",
+  "title": "",
+  "body": "  Classify each of given PDEs as hyperbolic, parabolic, or elliptic.                "
+},
+{
+  "id": "week7-handout-14",
+  "level": "2",
+  "url": "week7-handout.html#week7-handout-14",
+  "type": "Remark",
+  "number": "12",
+  "title": "",
+  "body": "  Here, we focus on finding product solutions of the 2nd-order PDEs and observe the relationship with Fourier series:  1-D Heat Equation:     1D Wave Equation:   Laplace's Equation: where or       "
+},
+{
+  "id": "def-heat",
+  "level": "2",
+  "url": "week7-handout.html#def-heat",
+  "type": "Definition",
+  "number": "13",
+  "title": "1-D Heat Equation.",
+  "body": " 1-D Heat Equation   Consider a thin rod of length with an initial temperature throughout and whose ends are held at temperature zero for all time   A thin rod .     The temperature in the rod is determined from the BVP:    "
 }
 ]
 
