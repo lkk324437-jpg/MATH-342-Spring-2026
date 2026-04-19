a5e802558b58074882d94c45d798d5679a614f39
lkk324437-jpg <lkk324437@gmail.com>
2026-04-19 16:40:57 +0000

    Latest build deployed.

--- week14/lunr-pretext-search-index.js
+++ week14/lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Worksheet",
   "number": "",
   "title": "Week 13-14 Handout-Integral Transforms",
-  "body": " Week 13-14 Handout-Integral Transforms    We will discuss the following topics related to Integral Transforms (Chap 15) :   Week 13: Laplace Transforms (15.2) , Fourier Transforms (15.4)    Week 14: Fourier Transforms (15.4), Finite Fourier Transforms (15.5)        Laplace Transform  The Laplace transform of with respect to is defined as  where is treated as a parameter.      In Week 1, we used to reduce the ODE to an algebraic equation. Here, we will use to reduce a PDE to an ODE. Here are a few useful properties:                    "
+  "body": " Week 13-14 Handout-Integral Transforms    We will discuss the following topics related to Integral Transforms (Chap 15) :   Laplace Transforms (15.2)    Fourier Transforms (15.4)        Laplace Transform  The Laplace transform of with respect to is defined as  where is treated as a parameter.      In Week 1, we used Laplace Transform to reduce the ODE to an algebraic equation. Here, we will use Laplace Transform to reduce a PDE to an ODE. Here are a few useful properties:                      Solve the boundary value problem using Laplace Transform:       Use the Laplace transform to solve the heat equation subjec to       Integral transforms appear in transform pairs. If is transformed into by an integral transform  then the function can be recovered by another integral transform called the inverse transform. The functions and in and are called kernels of their respective transforms. In Laplace transform , and      Fourier Transform Pairs         Fourier Transform :    Inverse Fourier Transform :             Fourier cosine Transform :    Inverse Fourier cosine Transform :             Fourier sine Transform :    Inverse Fourier sine Transform :             Existence and Properties of Fourier Transform     Sufficient conditions for existence of , , and are that be absolutely integrable on the appropriate interval and that and be piecewise continuous on every finite interval.      , and  do not exist.    All 6 transforms in are linear transforms.    Suppose that is continuous and absolute integrable on the interval and is piecewise continuous on every finite interval. If as then       With added assumptions that is continuous on , is piecewise continuous on every finite interval, and as , we have     Similarly, we have where        For Fourier Cosine Transform and Fourier Sine Transform : under the same assumptions mentioned above:                              Use a Fourier transform to solve:     "
 },
 {
   "id": "week14-handout-2-1-1",
@@ -34,7 +34,61 @@ var ptx_lunr_docs = [
   "type": "Remark",
   "number": "2",
   "title": "",
-  "body": "  In Week 1, we used to reduce the ODE to an algebraic equation. Here, we will use to reduce a PDE to an ODE. Here are a few useful properties:                   "
+  "body": "  In Week 1, we used Laplace Transform to reduce the ODE to an algebraic equation. Here, we will use Laplace Transform to reduce a PDE to an ODE. Here are a few useful properties:                   "
+},
+{
+  "id": "week14-handout-5",
+  "level": "2",
+  "url": "week14-handout.html#week14-handout-5",
+  "type": "Example",
+  "number": "3",
+  "title": "",
+  "body": "  Solve the boundary value problem using Laplace Transform:    "
+},
+{
+  "id": "week14-handout-6",
+  "level": "2",
+  "url": "week14-handout.html#week14-handout-6",
+  "type": "Example",
+  "number": "4",
+  "title": "",
+  "body": "  Use the Laplace transform to solve the heat equation subjec to    "
+},
+{
+  "id": "week14-handout-7",
+  "level": "2",
+  "url": "week14-handout.html#week14-handout-7",
+  "type": "Remark",
+  "number": "5",
+  "title": "",
+  "body": "  Integral transforms appear in transform pairs. If is transformed into by an integral transform  then the function can be recovered by another integral transform called the inverse transform. The functions and in and are called kernels of their respective transforms. In Laplace transform , and    "
+},
+{
+  "id": "def-fourier-transform",
+  "level": "2",
+  "url": "week14-handout.html#def-fourier-transform",
+  "type": "Definition",
+  "number": "6",
+  "title": "Fourier Transform Pairs.",
+  "body": " Fourier Transform Pairs         Fourier Transform :    Inverse Fourier Transform :             Fourier cosine Transform :    Inverse Fourier cosine Transform :             Fourier sine Transform :    Inverse Fourier sine Transform :           "
+},
+{
+  "id": "week14-handout-9",
+  "level": "2",
+  "url": "week14-handout.html#week14-handout-9",
+  "type": "Remark",
+  "number": "7",
+  "title": "Existence and Properties of Fourier Transform.",
+  "body": " Existence and Properties of Fourier Transform     Sufficient conditions for existence of , , and are that be absolutely integrable on the appropriate interval and that and be piecewise continuous on every finite interval.      , and  do not exist.    All 6 transforms in are linear transforms.    Suppose that is continuous and absolute integrable on the interval and is piecewise continuous on every finite interval. If as then       With added assumptions that is continuous on , is piecewise continuous on every finite interval, and as , we have     Similarly, we have where        For Fourier Cosine Transform and Fourier Sine Transform : under the same assumptions mentioned above:                           "
+},
+{
+  "id": "week14-handout-10",
+  "level": "2",
+  "url": "week14-handout.html#week14-handout-10",
+  "type": "Example",
+  "number": "8",
+  "title": "",
+  "body": "  Use a Fourier transform to solve:    "
 }
 ]
 
